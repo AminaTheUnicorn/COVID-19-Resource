3adc775514cd8fcdc5b95bd111dd813acac9952c
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-05 09:23:59 -0500

    news API and Covid 19 Location API retuning response

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
 var newsAPI = "4d2b9f062a9846ddb0e115c2ff221c20"
-var newsUrl = 'https://newsapi.org/v2/everything?domains=dallasnews.com,wfaa.com,fox4news.com,nbcdfw.com,star-telegram.com&q=covid-19&apiKey=' + newsAPI;
+var newsAPIDomains = "dallasnews.com,wfaa.com,fox4news.com,nbcdfw.com,star-telegram.com";
+var newsUrl = 'https://newsapi.org/v2/everything?pageSize=100&domains=' + newsAPIDomains + '&qInTitle=covid-19 OR(coronavirus)&apiKey=' + newsAPI;
 
 fetch(newsUrl)
 .then(function(response) {
 console.log(response.json());
 })
+
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://covid-19-testing.github.io/locations/texas/complete.json", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));

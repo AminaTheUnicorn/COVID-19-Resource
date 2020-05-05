546f6df73982daae2c095a4f7a7e42a4f5f7f910
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-05 12:45:07 -0500

    finished search query on news api for 10 most recent covid 19 articles in DFW new sourse

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 var newsAPI = "4d2b9f062a9846ddb0e115c2ff221c20"
 var newsAPIDomains = "dallasnews.com,wfaa.com,fox4news.com,nbcdfw.com,star-telegram.com,metroplexdaily.com,dallasobserver.com,texasmonthly.com,wfaa.com,texastribune.org,dallascityhall.com,dfw.cbslocal.com,texasstandard.org,thetexan.news,kera.org,dshs.state.tx.us";
-var newsUrl = 'https://newsapi.org/v2/everything?pageSize=100&domains=' + newsAPIDomains + '&qInTitle=covid-19 OR(coronavirus) OR(Coronavirus disease) OR(Coronavirus pandemic)&apiKey=' + newsAPI;
+var newsUrl = 'https://newsapi.org/v2/everything?sortBy=publishedAt&pageSize=10&qInTitle=+covid-19 OR(+coronavirus OR +corona-virus OR +disease OR +pandemic)&domains=' + newsAPIDomains + '&apiKey=' + newsAPI;
 
 fetch(newsUrl)
 .then(function(response) {

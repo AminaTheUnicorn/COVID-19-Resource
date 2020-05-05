695b232bcae5fa1aeffeb5f39f68b24bcc2b01ed
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-04 19:46:00 -0500

    connected to newsapi.org api and verified response

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-console.log("Aminat");
 
-console.log("Brian");
+var newsAPI = "4d2b9f062a9846ddb0e115c2ff221c20"
+var newsUrl = 'https://newsapi.org/v2/everything?domains=dallasnews.com,wfaa.com,fox4news.com,nbcdfw.com,star-telegram.com&q=covid-19&apiKey=' + newsAPI;
 
-console.log("Hannah")
+fetch(newsUrl)
+.then(function(response) {
+console.log(response.json());
+})

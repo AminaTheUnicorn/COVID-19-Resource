7c6c9b444cc89b39b2024d3931f1b1ae4867675e
Aminat.A <aminatadesola@gmail.com>
2020-05-05 09:42:55 -0500

    merging in master

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-console.log("Aminat");
 
-console.log("Brian");
 
-console.log("Hannah")
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://covid-19-testing.github.io/locations/:state/complete.json/phones/?format=json", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));

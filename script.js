095aaa5e14bb9c3a70c147a57f7cc2a336bfc6e4
Aminat.A <aminatadesola@gmail.com>
2020-05-06 03:33:11 -0500

    Added Clinic Drop list

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var newsAPI = "4d2b9f062a9846ddb0e115c2ff221c20"
 var newsAPIDomains = "dallasnews.com,wfaa.com,fox4news.com,nbcdfw.com,star-telegram.com,metroplexdaily.com,dallasobserver.com,texasmonthly.com,wfaa.com,texastribune.org,dallascityhall.com,dfw.cbslocal.com,texasstandard.org,thetexan.news,kera.org,dshs.state.tx.us";
 var newsUrl = 'https://newsapi.org/v2/everything?pageSize=100&domains=' + newsAPIDomains + '&qInTitle=covid-19 OR(coronavirus) OR(Coronavirus disease) OR(Coronavirus pandemic)&apiKey=' + newsAPI;
@@ -22,9 +21,36 @@ fetch("https://covid-19-testing.github.io/locations/texas/complete.json", reques
       if (dropDownList.indexOf(clinic.physical_address[0].city) === -1) {
         dropDownList.push(clinic.physical_address[0].city)
       }
-    
+
     });
-     console.log(dropDownList);
+    console.log(dropDownList);
+ 
+    var dropDownBtn = $(".dropdown-menu");
+    var dropList = $("<button>").addClass(".dropdown-item");
+    
+    dropDownList.forEach(function (listValue) {
+   
+   var button = document.createElement("button");
+   $("button").addClass("dropdown-item"); 
+   button.innerHTML = listValue;
+    
+
+    $(".dropdown-menu").append(button);
+
+
+    });
+
+
+    // var matches = document.querySelector("dropDownList");
+    // dropDownList.forEach(function() {
+    //   if()
+    // }
+
+    // for (var i = 0; i < dropDownList.length; i++){
+    //   var dropList = $("<button>").addClass("dropdown-item");
+    //   $("dropList").append(dropDownList)
+
+    // }
+
   })
   .catch(error => console.log('error', error));
-
